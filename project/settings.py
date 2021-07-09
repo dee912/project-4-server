07a6065569aa89bebcd736e0c7bcdb28becda045
dee912 <devantespaine@gmail.com>
2021-07-09 10:14:43 +0100

    deploy

--- project/settings.py
+++ project/settings.py
@@ -10,10 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import django_on_heroku
-
 import os
 from pathlib import Path
+import django_on_heroku
 from dotenv import load_dotenv
 import dj_database_url
 load_dotenv()
